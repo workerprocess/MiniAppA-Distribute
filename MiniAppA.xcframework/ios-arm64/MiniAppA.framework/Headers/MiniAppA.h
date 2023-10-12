f86b8738ac143b6bbf23a3ef3ced343548ed206e
workerprocess <workerprocess@gmail.com>
2023-10-13 01:09:28 +0700

    Initial commit

---
+++ MiniAppA.xcframework/ios-arm64/MiniAppA.framework/Headers/MiniAppA.h
@@ -0,0 +1,18 @@
+//
+//  MiniAppA.h
+//  MiniAppA
+//
+//  Created by ekachai mankongtongjaroen on 10/10/2566 BE.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MiniAppA.
+FOUNDATION_EXPORT double MiniAppAVersionNumber;
+
+//! Project version string for MiniAppA.
+FOUNDATION_EXPORT const unsigned char MiniAppAVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MiniAppA/PublicHeader.h>
+
+
---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.7
+import PackageDescription
+let package = Package(
+    name: MiniAppA,
+    platforms: [
+        .iOS(.v16)
+    ],
+    products: [
+        .library(
+            name: MiniAppA,
+            targets: [MiniAppA])
+    ],
+    targets: [
+        .binaryTarget(
+            name: MiniAppA,
+            path: "MiniAppA.xcframework")
+    ])
