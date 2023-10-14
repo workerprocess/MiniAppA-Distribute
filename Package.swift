ce55750302ba9a5a56da9602247b07962967a172
workerprocess <workerprocess@gmail.com>
2023-10-14 15:17:06 +0700

    Add title param

--- Package.swift
+++ Package.swift
@@ -8,16 +8,10 @@ let package = Package(
     products: [
         .library(
             name: "MiniAppA",
-            targets: ["MiniAppA","LogicCalculate"])
+            targets: ["MiniAppA"])
     ],
     targets: [
         .binaryTarget(
             name: "MiniAppA",
-            path: "MiniAppA.xcframework"
-        )
-        .binaryTarget(
-            name: "LogicCalculate",
-            url: "https://s3.ap-southeast-1.amazonaws.com/storage.default/LogicCalculate.zip",
-            checksum: "9c91b2092d59a1a62f0635952a06859862cf66eb05458c907f40d3978e56e432"
-        )
+            path: "MiniAppA.xcframework")
     ])
